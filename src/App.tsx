000a90f254f5c33cb6c79394ee96aa2798722e62
Nico Poggi <73345016+nicolaspoggi@users.noreply.github.com>
2023-09-27 16:24:47 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,3 @@
-import reactLogo from './assets/react.svg'
-import fleekLogo from './assets/fleekLogo.svg'
-import fleekMark from './assets/fleekMark.svg'
-import plusIcon from './assets/plus.svg'
-import viteLogo from './assets/vite.svg'
 import useState from 'react'
 
 function App() {
